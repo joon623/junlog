d94d25ed072fe97b5467493840bf314c3ee8109e
joon623 <whitebear@junui-iMac.local>
2022-02-10 22:04:52 +0900

    first

---
+++ content/posts/hello.md
@@ -0,0 +1,7 @@
+---
+title: Hello World (example)
+date: 2019-04-15
+image: ./some-image.jpg
+---
+
+Posts should be written from here
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,41 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
+// gatsby-config.js
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/blog`,
+      },
+    },
+  ],
+  siteMetadata: {
+    // Used for the site title and SEO
+    title: `My Blog Title`,
+    // Used to provide alt text for your avatar
+    author: `My Name`,
+    // Used for SEO
+    description: `My site description...`,
+    // Used for resolving images in social cards
+    siteUrl: `https://example.com`,
+    // Used for social links in the root footer
+    social: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `GitHub`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        prismPreset: `dracula`,
+      },
+    },
+  ],
 }
